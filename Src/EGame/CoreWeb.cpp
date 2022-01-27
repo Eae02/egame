9785e3443f1682fd47055cd473a5c02338284cd3
Erik Amirell Eklöf <erik.amirell.eklof@gmail.com>
2022-01-27 11:59:20 +0100

    added enter to non text keys

--- Src/EGame/CoreWeb.cpp
+++ Src/EGame/CoreWeb.cpp
@@ -119,7 +119,7 @@ namespace eg
 	{
 		"F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15", "F16",
 		"F17", "F18", "F19", "F20", "F21", "F22", "F23", "Tab", "Backspace", "ArrowLeft", "ArrowUp", "ArrowRight",
-		"ArrowDown", "PageUp", "PageDown", "Home", "End", "Delete", "CapsLock", "AltGraph"
+		"ArrowDown", "PageUp", "PageDown", "Home", "End", "Delete", "CapsLock", "AltGraph", "Enter"
 	};
 	
 	static std::string newInputtedText;
